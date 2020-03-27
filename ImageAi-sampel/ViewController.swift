3cf5c2ab797eca804d6b191ab8f2bf16ec988b68
a1824jj <a1824jj@aiit.ac.jp>
2020-03-27 17:17:11 +0900

    make ui

---
+++ ImageAi-sampel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageAi-sampel
+//
+//  Created by JONGWOO JIN on 2020/03/27.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var photoDisplay: UIImageView!
+    @IBOutlet weak var photoInfoDisplay: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func takePhoto(_ sender: Any) {
+    }
+    
+}
+
